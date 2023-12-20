9ab8bf81164e395b6406482102d9c62bae2526a9
Jacob <jacob.chiu168@hotmail.com>
2023-12-20 15:03:44 -0500

    done

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import requests
-import json
 
 url = 'https://api.discord.gx.games/v1/direct-fulfillment'
 headers = {
@@ -17,25 +16,37 @@ headers = {
     'sec-fetch-site': 'cross-site',
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 OPR/105.0.0.0',
 }
-
 data = {
     'partnerUserId': 'd5c4204872690962c2a3d9ebcf86e7712341efc8bc43727611940a6a08c85bda'
 }
-
 preTxt = 'https://discord.com/billing/partner-promotions/1180231712274387115/'
 
-runs = int(input(f'Runs: '))
-collection = []
-for _ in range(runs):
-    response = requests.post(url, headers=headers, json=data)
-    print(f'Status: {response.status_code}\nResponse: {response.text}\n')
+print(f'''   
+ _   _ _____ _______ _____   ____    _____   ____   _____ _______ 
+| \ | |_   _|__   __|  __ \ / __ \  |  __ \ / __ \ / ____|__   __|
+|  \| | | |    | |  | |__) | |  | | | |__) | |  | | (___    | |   
+| . ` | | |    | |  |  _  /| |  | | |  ___/| |  | |\___ \   | |   
+| |\  |_| |_   | |  | | \ \| |__| | | |    | |__| |____) |  | |   
+|_| \_|_____|  |_|  |_|  \_\\____/  |_|     \____/|_____/   |_|                
+By: MessyToilet''')
+input("\n( Enter ) to begin...")
 
-    data_dict = response.json()
-    token_value = data_dict.get('token', None)
+for i in range(1, 100):
+    file   = 'nitro_posts{i}.txt'
+    runs = 48
+    collection = []
+    for _ in range(runs):
+        response = requests.post(url, headers=headers, json=data)
+        print(f'Status: {response.status_code}\nResponse: {response.text}\n')
 
-    print(f'Stuffing: {token_value}\n\n')
-    collection.append(preTxt + token_value)
+        data_dict = response.json()
+        token_value = data_dict.get('token', None)
 
-with open('nitro_posts2.txt', 'w') as FILE:
-    for token in collection:
-        FILE.write(token + '\n')
+        print(f'Stuffing: {token_value}\n')
+        collection.append(preTxt + token_value)
+
+    with open(file, 'w') as FILE:
+        FILE.write(f'Keys: {runs}\n')
+        for token in collection:
+            FILE.write(token + '\n\n')
+print(f'\n\n------ ! (48 x 100) NITRO KEYS GENERATED ! ------')
